0bafb49a9b6f20c176055f72878e67e47f4de50d
Peter de Boer <peterdeboer_private@hotmail.com>
2018-10-16 17:33:47 +0000

    added SQLALCHEMY_TRACK_MODIFICATIONS = False

--- ponzischeme/config.py
+++ ponzischeme/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
