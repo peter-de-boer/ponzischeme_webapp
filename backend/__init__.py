805d77b0772705a51d252ca315b0015f2d534f56
Peter de Boer <peterdeboer_private@hotmail.com>
2018-11-01 20:47:38 +0100

    added flask-migrate activated sqlalchemy

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_cors import CORS
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from flask_bcrypt import Bcrypt
 #from flask_login import LoginManager
 #from flask_mail import Mail
@@ -8,7 +8,7 @@ from backend.config import Config
 import requests
 
 
-#db = SQLAlchemy()
+db = SQLAlchemy()
 #bcrypt = Bcrypt()
 #login_manager = LoginManager()
 #login_manager.login_view = 'users.login'
@@ -21,7 +21,7 @@ def create_app(config_class=Config):
     CORS(app)
     app.config.from_object(Config)
 
-    #db.init_app(app)
+    db.init_app(app)
     #bcrypt.init_app(app)
     #login_manager.init_app(app)
     #mail.init_app(app)
