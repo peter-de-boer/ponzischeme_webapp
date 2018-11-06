ad520c48544dc77e097fcd1c33af7af8334cbe3a
Peter de Boer <peterdeboer_private@hotmail.com>
2018-11-06 09:40:19 +0100

    added header and otherroute

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul class="nav nav-pills">
+        <li role="presentation"><router-link to="/">Home</router-link></li>
+        <li role="presentation"><router-link to="/urlgame">Game</router-link></li>
+        <li role="presentation"><router-link to="/otherroute">Other Route</router-link></li>
+    </ul>
+</template>
---
+++ frontend/src/components/OtherRoute.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h3>This is another route</h3>
+    </div>
+</template>
+
+<script>
+</script>
