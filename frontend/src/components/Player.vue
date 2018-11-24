2dcc01829e1091f48011b2650e2b5a80ded58930
Peter de Boer <peterdeboer_private@hotmail.com>
2018-11-24 10:15:37 +0100

    hilighting

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="player" :class="hiLight(player.name, selectedPlayerAndTile)">
         <player-industry-tiles :industryTiles="player.industryTiles" :name="player.name"></player-industry-tiles>
         <time-wheel :wheel="player.wheel"></time-wheel>
         <p>name:  {{player.name}} </p>
@@ -11,13 +11,46 @@
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
     import TimeWheel from './TimeWheel.vue'
     import PlayerIndustryTiles from './PlayerIndustryTiles.vue'
+
     export default {
         props: ['player'],
+        computed: {
+            ...mapGetters([
+                'selectedPlayerAndTile'
+            ])
+        },
         components: {
             timeWheel: TimeWheel,
             playerIndustryTiles: PlayerIndustryTiles
+        },
+        methods: {
+            hiLight(name, sel) {
+                if (sel && name==sel.name) {
+                    return "hilight";
+                } else {
+                    return "nohilight";
+                }
+                    
+            }
         }
     }
 </script>
+
+<style scoped>
+
+.player {
+    border-style: inset;
+    border-width: 2px;
+}
+
+.hilight {
+}
+
+.nohilight {
+    border-color: transparent;
+}
+
+</style>
