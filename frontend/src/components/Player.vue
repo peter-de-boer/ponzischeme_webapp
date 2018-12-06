e7e5ab79e37d1ba09e4eddadb0b54a5818db3c53
Peter de Boer <peterdeboer_private@hotmail.com>
2018-12-06 20:26:04 +0100

    moved luxury tiles

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -3,6 +3,8 @@
         :class="hiLight(player.name, selectedPlayerAndTile)">
         <player-industry-tiles :industryTiles="player.industryTiles" :name="player.name"></player-industry-tiles>
 
+        <time-wheel :wheel="player.wheel"></time-wheel>
+        <p>name:  {{player.name}} money: {{player.money}} </p>
         <div class="row">
             <div class="col-sm-3" v-for="(tile, i) in player.luxuryTiles" :key="i">
                 <div class="tile" >
@@ -11,8 +13,6 @@
             </div>
         </div>
 
-        <time-wheel :wheel="player.wheel"></time-wheel>
-        <p>name:  {{player.name}} money: {{player.money}} </p>
         <hr>
     </div>
 </template>
