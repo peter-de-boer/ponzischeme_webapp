f6534920a5026856605437396bbc8364c02b4f14
Peter de Boer <peterdeboer_private@hotmail.com>
2019-02-13 16:39:29 +0100

    frontend create game

---
+++ frontend/src/components/CreateGame.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        Number of players:
+        <input type="radio" name="npl" value=3 v-model.number="nplayers"  checked> 3
+        <input type="radio" name="npl" value=4 v-model.number="nplayers" > 4
+        <input type="radio" name="npl" value=5 v-model.number="nplayers" > 5<br>
+        Advanced <input type="checkbox" id="adv" v-model="advanced"> <br>
+        <button @click="createGame(nplayers,advanced)">Create</button>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+        data: function() {
+            return {
+                nplayers: 3,
+                advanced: false
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'token',
+                'isAuthenticated'
+            ])    
+        },
+        methods: {
+            createGame(nplayers,advanced) {
+                if (this.token) {
+                    var json = {"nplayers": nplayers, "advanced": advanced, "token": this.token}
+                    //axios.put('/creategame', json)
+                    //    .then( res => {
+                    //        console.log(res)
+                    //}, error => {
+                    //    console.log(error)
+                    //}); 
+                }
+                else {
+                    console.log("no token")
+                }
+            }
+        }
+    }    
+</script>
+
+<style scoped>
+</style>
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-            <li class="nav-item mx-2"><router-link to="/urlgame">Game</router-link></li>
-  </div>
+    <div>
+        <li class="nav-item mx-2"><router-link to="/urlgame">Game</router-link></li>
+        <hr>
+        <create-game v-if="isAuthenticated"></create-game>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+    import { mapGetters } from 'vuex';
+    import CreateGame from './CreateGame.vue';
+
+    export default {
+        name: 'Home',
+        computed: {
+            ...mapGetters([
+                'isAuthenticated'
+            ]),   
+        },
+        components: {
+            createGame: CreateGame
+        }
+    }
 </script>
 
 <style scoped>
