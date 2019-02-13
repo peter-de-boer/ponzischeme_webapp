5d3accc4763537f3a3145ee88a93752010492b8f
Peter de Boer <peterdeboer_private@hotmail.com>
2019-02-13 15:17:40 +0100

    cleanup

--- frontend/src/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <nav class="navbar navbar-expand bg-light">
-        <ul class="navbar-nav">
-            <li class="nav-item mr-2"><router-link to="/">Home</router-link></li>
-            <li class="nav-item mx-2"><router-link to="/urlgame">Game</router-link></li>
-            <li class="nav-item mx-2"><router-link to="/otherroute">Other Route</router-link></li>
-        </ul>
-    </nav>
-</template>
--- frontend/src/components/OtherRoute.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <h3>This is another route</h3>
-    </div>
-</template>
-
-<script>
-</script>
