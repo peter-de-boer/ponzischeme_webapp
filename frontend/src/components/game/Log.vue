0a5073179d5a939c6966830c26d09a79e3bc2a5f
Peter de Boer <peterdeboer_private@hotmail.com>
2019-02-22 20:58:31 +0100

    workaround for scroll down

--- frontend/src/components/game/Log.vue
+++ frontend/src/components/game/Log.vue
@@ -1,24 +1,41 @@
 <template>
-    <div class="log"> 
+    <div id="log" ref="myid" class="log"> 
         <!--
-            need an additional div to get the scroll 
+            for flex-direction: column-reverse; need an additional div to get the scroll 
             down to bottom feature working 
             https://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
+            
+            but it appears the column-reverse does not work on all browsers (e.g firefox)
+            so additional div is removed
+            instead scrollTop=scrollHeight is used
+            but that somehow does not work as is, seems to be related with rendering process 
+            only with a sufficient timeout it works
         -->
-        <div>
             <h2> Log </h2>
             <div v-for="line in log"> {{line}} </div>
-        </div>
     </div>
 </template>
 
 <script>
     import { mapGetters } from 'vuex';
     export default {
+        mounted() {
+            this.scrollToEnd()
+        },
         computed: {
             ...mapGetters([
-                'log'
-            ])    
+                'log',
+                'gameStateLoaded'
+            ])
+        },
+        methods: {
+            scrollToEnd() {
+              var self = this
+              setTimeout(function() {
+                var container = self.$refs["myid"];
+                container.scrollTop = container.scrollHeight;
+              },200)
+            }
         }
     }    
 </script>
@@ -28,7 +45,9 @@
     border-style: solid;
     height: 350px;
     overflow: auto;
+    /*
     display: flex;
     flex-direction: column-reverse;
+    */
 }
 </style>
