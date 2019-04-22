f879dab9e89f2058701a5549e5a7e93fb19772a5
Peter de Boer <peterdeboer_private@hotmail.com>
2019-04-22 08:44:35 +0200

    add Notes.vue

---
+++ frontend/src/components/game/Notes.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="notes">
+        <h2> Notes </h2>
+        <div ref="myid" class="nnotes"> 
+                {{gameNotes}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import { mapActions } from 'vuex';
+    import { mapGetters } from 'vuex';
+
+    export default {
+        data () {
+            return {
+                post: '',
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'id',
+                'token',
+                'gameNotes',
+                'gameStateLoaded',
+                'username'
+            ])
+        },
+        methods: {
+            ...mapActions([
+                'setGameState'
+            ])
+        }
+    }    
+</script>
+
+<style scoped>
+.notes {
+    border-style: solid;
+    height: 350px;
+}
+.nnotes {
+    height: 295px;
+    width: 98%;
+    margin: 0 auto;
+    overflow: auto;
+    background: white;
+}
+</style>
