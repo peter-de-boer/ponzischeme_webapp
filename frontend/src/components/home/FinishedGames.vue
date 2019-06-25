2d1934cfa40ec1f0fa968f8baf7ad729ed9bc526
Peter de Boer <peterdeboer_private@hotmail.com>
2019-06-25 12:48:24 +0200

    working on pagination

--- frontend/src/components/home/FinishedGames.vue
+++ frontend/src/components/home/FinishedGames.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
         Finished Games <br>
+        {{finishedGames}}
         <div v-for="(game, index) in finishedGames" :key="index">
             <finished-game :game="game"></finished-game>
         </div>
+        <button @click="prevPage" :disabled="pageNumber==0">
+            Previous
+        </button>
+        <button @click="nextPage" :disabled="pageNumber >= pageCount -1">
+            Next
+        </button>
     </div>
 </template>
 
@@ -12,10 +19,35 @@
     import FinishedGame from './FinishedGame.vue';
 
     export default {
+        data() { 
+            return { 
+                pageNumber: 0,  
+                size: 10
+            } 
+        },
+        methods: { 
+            nextPage() { 
+                this.pageNumber++; 
+            }, 
+            prevPage() { 
+                this.pageNumber--; 
+            } 
+        },
         computed: {
             ...mapGetters([
                 'finishedGames'
-            ])    
+            ]),
+            pageCount() { 
+                let l = this.finishedGames.length, 
+                    s = this.size; 
+                return Math.ceil(l/s); 
+            },
+            paginatedData() { 
+                const start = this.pageNumber * this.size, 
+                      end = start + this.size; 
+                console.log(this.finishedGames);
+                return this.finishedGames.slice(start, end); 
+            }
         },
         components: {
             finishedGame: FinishedGame
