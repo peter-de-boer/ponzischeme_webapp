74bac9fd7d06008bb56bfa0c9bba6edd815357ff
Peter de Boer <peterdeboer_private@hotmail.com>
2022-09-17 08:53:14 +0200

    moved email server to web.de

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.web.de'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
